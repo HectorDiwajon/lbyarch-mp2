392dd27b0d8431035d2390f194f93c1f3398eec7
Neo de Guzman <neoxdg@gmail.com>
2024-11-27 14:50:32 +0800

    added comments

--- lbyarch-mco2.asm
+++ lbyarch-mco2.asm
@@ -7,19 +7,15 @@ default rel
 global imgCvtGrayInttoFloat
 
 imgCvtGrayInttoFloat:
-    mov rbx, r9
-    cvtsi2ss xmm1, rbx
+    mov rbx, r9                 ; rbx has 255
+    cvtsi2ss xmm1, rbx          ; make rbx to float
     L1:
+        mov eax, dword [rdx]    ; intialize array value
+        cvtsi2ss xmm0, eax      ; make it to float
+        divss xmm0, xmm1        ; divide it by 255
+        movss dword [r8], xmm0  ; put the value to the float array
         
-        
-        mov eax, dword [rdx]
-        
-        cvtsi2ss xmm0, eax
-        divss xmm0, xmm1
-        
-        movss dword [r8], xmm0
-        
-        add rdx, 4
+        add rdx, 4              ; move by 1 index
         add r8, 4
         loop L1
     ret
